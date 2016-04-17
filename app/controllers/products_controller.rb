c36f173c55837855dda3663210e2dfa540b34522
Chris Bon <chrisbon315@gmail.com>
2016-04-17 15:27:24 -0700

    Add redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,15 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
-  end
-
   def show
     @product = Product.find_by id: params[:id]
   end
 
   def create
-    Product.new(name: params[:name],  price: params[:price],
-               image: params[:image], description: params[:description]).save
+    product = Product.new name: params[:name],  
+                         price: params[:price],
+                         image: params[:image], 
+                   description: params[:description]
+    product.save
+    redirect_to "/products/#{product.id}"
   end
 
   def edit
@@ -18,14 +18,9 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find_by id: params[:id]
-    @product.update(name: params[:name],  price: params[:price],
-                   image: params[:image], description: params[:description])
-    #@product = Product.find_by(id: params[:id])
-    #@product.name = params[:name]
-    #@product.price = params[:price]
-    #@product.image = params[:image]
-    #@product.description = params[:description]
-    #@product.save
+    @product.update name: params[:name],        price: params[:price],
+                   image: params[:image], description: params[:description]
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
