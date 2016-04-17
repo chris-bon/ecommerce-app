cc983b6ae1b1c73285fdc868046df8e0fc9437bc
Chris Bon <chrisbon315@gmail.com>
2016-04-17 14:11:30 -0700

    Fix Update and Add Links

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,23 +4,22 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find_by id: params[:id]
   end
 
   def create
-    Product.new( name: params[:name],        price: params[:price], 
-                image: params[:image], description: params[:description]).save
+    Product.new(name: params[:name],  price: params[:price],
+               image: params[:image], description: params[:description]).save
   end
 
   def edit
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find_by id: params[:id]
   end
 
   def update
-    Product.find_by(id: params[:id]).update( name: params[:name],
-                                            price: params[:price],
-                                            image: params[:image], 
-                                      description: params[:description])
+    @product = Product.find_by id: params[:id]
+    @product.update(name: params[:name],  price: params[:price],
+                   image: params[:image], description: params[:description])
     #@product = Product.find_by(id: params[:id])
     #@product.name = params[:name]
     #@product.price = params[:price]
@@ -30,6 +29,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    Product.find_by(id: params[:id]).destaroy
+    @product = Product.find_by id: params[:id]
+    @product.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root   'products#index'
-  get    'products'          => 'products#index'
-  get    'products/new'      => 'products#new'
-  post   'products'          => 'products#create'  
-  get    'products/:id'      => 'products#show'
-  get    'products/:id/edit' => 'products#edit'
-  patch  'products/:id'      => 'products#update'
-  delete 'products/:id'      => 'products#destroy'
+  get    '/products'          => 'products#index'
+  get    '/products/new'      => 'products#new'
+  post   '/products'          => 'products#create'  
+  get    '/products/:id'      => 'products#show'
+  get    '/products/:id/edit' => 'products#edit'
+  patch  '/products/:id'      => 'products#update'
+  delete '/products/:id'      => 'products#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
