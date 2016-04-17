98dbf06ba46b013cba06bca4f62121694da048f5
Chris Bon <chrisbon315@gmail.com>
2016-04-17 13:29:38 -0700

    Add more RESTful routes with actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,30 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def create
+    Product.new( name: params[:name],        price: params[:price], 
+                image: params[:image], description: params[:description]).save
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    Product.find_by(id: params[:id]).update( name: params[:name],
+                                            price: params[:price],
+                                            image: params[:image], 
+                                      description: params[:description])
+    #@product = Product.find_by(id: params[:id])
+    #@product.name = params[:name]
+    #@product.price = params[:price]
+    #@product.image = params[:image]
+    #@product.description = params[:description]
+    #@product.save
+  end
+
+  def destroy
+    Product.find_by(id: params[:id]).destaroy
+  end
 end
